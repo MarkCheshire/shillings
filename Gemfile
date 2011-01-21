d37158a5b423b8a146491f241f9124764e3ae305
Mark Cheshire <Mark_Cheshire@alum.mit.edu>
2011-01-21 11:50:37 +0100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+
+group :development do
+  gem 'rspec-rails'
+  gem 'annotate-models'
+  gem 'faker'
+end
+
+group :test do
+  gem 'rspec'
+  gem 'webrat'
+  gem 'factory_girl_rails'
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shillings::Application.config.secret_token = '52dd3a12a14bc1cc1e0ebdd249a25704c1f672506f484c7b378b4bbaedf8010c3fee9a1eb5f893f51b64cba00376d6d5fcb3653ec238aba8d254d802bd4406f1'
